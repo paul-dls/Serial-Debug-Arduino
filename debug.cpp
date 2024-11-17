f1bd902cd2118449193cd54876446526154a3360
paul-dls <80622713+paul-dls@users.noreply.github.com>
2024-11-17 09:02:10 -0800

    add files using Streams (not tested)

--- debug.cpp
+++ debug.cpp
@@ -1,28 +1,38 @@
 
 #include "debug.h"
 
-void debug_println(String x, int debug_level) {
+Debug::Debug(Stream* _stream, uint8_t _debug_level) {
+  stream = _stream;
+  debug_level = _debug_level;
+}
+
+void Debug::setDebugLevel(uint8_t _debug_level) {
+  debug_level = _debug_level;
+}
+
+void Debug::println(String x, int debug_level) {
   //a function to print debug messages only if they are ob=ver the DEBUG_LEVEL
 
   if (debug_level <= DEBUG_LEVEL) {
     if (debug_level == DEBUG_WARNING) {
-      Serial.print("\033[33m"); // Set text color to yellow
+      stream->print("\033[33m"); // Set text color to yellow
     } else if (debug_level == DEBUG_ERROR) {
-      Serial.print("\033[31m"); // Set text color to red
+      stream->print("\033[31m"); // Set text color to red
     }
-    Serial.print(x);
-    Serial.println("\033[0m"); // Reset text color
+    stream->print(x);
+    stream->println("\033[0m"); // Reset text color
   }
 }
-void debug_print(String x, int debug_level) {
+
+void Debug::print(String x, int debug_level) {
   if (debug_level <= DEBUG_LEVEL) {
         if (debug_level == DEBUG_WARNING) {
-      Serial.print("\033[33m"); // Set text color to yellow
+      stream->print("\033[33m"); // Set text color to yellow
     } else if (debug_level == DEBUG_ERROR) {
-      Serial.print("\033[31m"); // Set text color to red
+      stream->print("\033[31m"); // Set text color to red
     }
-    Serial.print(x);
-    Serial.print("\033[0m"); // Reset text color
+    stream->print(x);
+    stream->print("\033[0m"); // Reset text color
 
   }
 }
--- debug.h
+++ debug.h
@@ -9,10 +9,65 @@
 #define DEBUG_ERROR 1
 #define DEBUG_NONE 0
 
+
+
 #define DEBUG_LEVEL DEBUG_NORMAL
 
-void debug_println(String x, int debug_level);
+class Debug : public Stream{
+    public:
+        Debug(Stream* stream, uint8_t debug_level = DEBUG_NORMAL);
 
-void debug_print(String x, int debug_level);
+        void print(String x, int debug_level=DEBUG_NORMAL);
+        void println(String x, int debug_level=DEBUG_NORMAL);
+
+        void print(int x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(int x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(float x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(float x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(double x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(double x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(uint8_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(uint8_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(uint16_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(uint16_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(uint32_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(uint32_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(int8_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(int8_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(int16_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(int16_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(int32_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(int32_t x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void print(char x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+        void println(char x, int debug_level=DEBUG_NORMAL){print((String)x, debug_level);};
+
+        void println(){println("", debug_level);};
+        void print(){print("", debug_level);};
+
+        void setDebugLevel(uint8_t debug_level);
+
+        int available(){return stream->available();};
+        int read(){return stream->read();};
+        int peek(){return stream->peek();};
+        size_t readBytesUntil(char terminator, char *buffer, size_t length){return stream->readBytesUntil(terminator, buffer, length);};
+        size_t readBytes(char *buffer, size_t length){return stream->readBytes(buffer, length);}; 
+        void flush(){stream->flush();};
+        size_t write(uint8_t c){return stream->write(c);};
+
+
+    private:
+        uint8_t debug_level;
+        Stream* stream;
+};
+
 
 #endif
